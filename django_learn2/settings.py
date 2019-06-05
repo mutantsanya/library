4d3251cd3cbacf2d06a7a187341c5327f4d42c59
mutantsanya <mutantsan@gmail.com>
2019-06-05 18:30:39 +0300

    created catalog app

--- django_learn2/settings.py
+++ django_learn2/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_learn2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'library',
+        'USER': 'postgres',
+        'PASSWORD': 1,
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
@@ -104,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
